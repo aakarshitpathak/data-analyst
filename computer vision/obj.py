f6432e6020d7ef6085f8641c0a2e34fefbafaa52
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-08-27 00:07:03 +0530

    commit67

---
+++ computer vision/obj.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+# pre-trained YOLOv8 Nano model (fast aur lightweight)
+model = YOLO("yolov8n.pt")
+
+# Webcam se live object detection (source=0 matlab default camera)
+model.predict(
+    source = 0, #webcam
+    show = True, # Live window will show
+    conf = 0.5 #confidence threshold  (50%)
+)
+
