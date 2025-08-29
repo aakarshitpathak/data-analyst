9f8dd2ad3c93c3ef3a065576fd7352af0f2964fb
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-08-30 00:06:14 +0530

    commit70

---
+++ Dash/home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center; color: white; border: 2px solid yellow;"\
+            "background-color: black;'>Car Crashes Dashboard</h1>", unsafe_allow_html=True)
+
+
+st.image("assets/img.webp")
+
+st.subheader("Savdhani Hati Sabji Pudi Bati")       
