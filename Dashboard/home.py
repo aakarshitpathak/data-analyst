85e934e0ae4f21c44f17e334abe7c196f3856fb3
Aakarshit Pathak <aakarshitpathaka@gmail.com>
2025-08-31 00:04:07 +0530

    commit77

---
+++ Dashboard/home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.markdown("<h1 style='text-align: center; color: black; \
+            border: 2px solid yellow;"\
+            "background-color: green;'>Hidden Insights Of Titanic Incidents</h1>", unsafe_allow_html=True)
+
+st.image("assets/titanic.png")
